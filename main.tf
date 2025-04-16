b15eeb499c01923d9192a3a4418537ae307685c5
dashath0097 <dashathk0440@gmail.com>
2025-04-16 10:41:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "tfe_workspace" "this" {
   force_delete      = true
 
   vcs_repo {
-    identifier     = "dashath0097/prompt5"
+    identifier     = "dashath0097/promt5"
     branch         = "main"
     oauth_token_id = tfe_oauth_client.this.oauth_token_id
   }
@@ -71,7 +71,7 @@ resource "tfe_workspace" "one_eight_one" {
   force_delete      = true
 
   vcs_repo {
-    identifier     = "apollorion/simple-module"
+    identifier     = "dashath0097/promt5"
     branch         = "main"
     oauth_token_id = tfe_oauth_client.this.oauth_token_id
   }
